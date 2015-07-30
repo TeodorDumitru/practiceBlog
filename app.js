5112219036d2e09276a3f907d1d6fcfc90f481a6
sallas <tbc105@hotmail.com>
2015-07-30 16:16:22 +0200

    Bug fix db connection

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var db = require("./model/db");
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var db = "mongodb://icanteven:icanteven@ds047642.mongolab.com:47642/practiseblog";
+
 var app = express();
 
 // view engine setup
